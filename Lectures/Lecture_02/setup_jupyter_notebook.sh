25ae0f3eeb36b516c992dbe77e572f93ac2f9aa8
edunford <edunford@umd.edu>
2018-09-10 10:50:41 -0400

    added small shell script w/ guidance on getting jupyter notebooks setup

---
+++ Lectures/Lecture_02/setup_jupyter_notebook.sh
@@ -0,0 +1,8 @@
+# Import list for Jupyer
+pip3 install jupyter # install Jupyter notebooks (rather than download anaconda)
+pip3 install jupyter_contrib_nbextensions # (download notebook extensions)
+
+# activate extensions
+jupyter contrib nbextension install --user
+
+# For more information on activating/deactivating specific extensions, see: https://github.com/ipython-contrib/jupyter_contrib_nbextensions
